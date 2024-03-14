29c15420142ec98d909b43dd12a2bcbe72f958d5
Rodrigo Batini <rodrigobatini@gmail.com>
2024-03-14 02:21:27 -0300

    enabling JWT as a mechanism for authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,7 @@ import { UsersModule } from './users/users.module';
     UsersModule,
 
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async signIn(username: string, pass: string,): Promise<{ access_token: string }> {
+    const user = await this.usersService.findOne(username);
+    //TODO INCLUDE BCRYPT OR ANOTHER SALT STRATEGY TO HANDLE PASSWORDS
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const payload = { sub: user.userId, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
